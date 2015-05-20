e55a16cd5c5dea777847992ce706f0f81873dfe3
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-05-20 12:18:57 -0400

    Initial commit

---
+++ lang/en/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Name: lang.php
+ * Description: The English language localization file for door43obsdocupload plugin.
+ *
+ * Author: Phil Hopper
+ * Date:   2015-05-20
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+//$lang['translateTitle'] = 'Translate';
+
+// localized strings for JavaScript
+// js example: var text = LANG.plugins['door43register']['translate'];
+//$lang['js']['translate'] = 'Translate';
+
